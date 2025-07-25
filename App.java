46f08b8a1a21a6073a69405bc992217c1a7dca81
mohammedsbd <mohammed1112ok@gmail.com>
2025-07-25 05:30:30 -0700

    setup database connection

---
+++ App.java
@@ -0,0 +1,12 @@
+// import university.conn;
+
+// public class App {
+//     public static void main(String[] args) {
+//         conn connection = new conn();
+//         if (connection.c != null) {
+//             System.out.println("Database connected successfully!");
+//         } else {
+//             System.out.println("Failed to connect to database.");
+//         }
+//     }
+// }
