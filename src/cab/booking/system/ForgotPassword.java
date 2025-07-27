44069cc290a91dc0f293bf630ac20b235a8e488b
mohammedsbd <mohammed1112ok@gmail.com>
2025-07-27 08:32:44 -0700

    add three buttons for search retrive andback

--- src/cab/booking/system/ForgotPassword.java
+++ src/cab/booking/system/ForgotPassword.java
@@ -88,5 +88,38 @@ public class ForgotPassword extends JFrame implements ActionListener{
 	contentPane.add(t5);
 
 
+     ImageIcon c1 = new ImageIcon(ClassLoader.getSystemResource("cab/booking/system/icons/forgotpassword.jpg"));
+        Image i1 = c1.getImage().getScaledInstance(200, 200,Image.SCALE_DEFAULT);
+        ImageIcon i2 = new ImageIcon(i1);
+        
+        JLabel l6 = new JLabel(i2);
+        l6.setBounds(560, 70, 200, 200);
+        add(l6);
+
+        b1 = new JButton("Search");
+	b1.addActionListener(this);
+	b1.setFont(new Font("Tahoma", Font.BOLD, 12));
+	b1.setBounds(428, 83, 80, 29);
+	b1.setBackground(Color.BLACK);
+        b1.setForeground(Color.WHITE);
+        contentPane.add(b1);
+
+	b2 = new JButton("Retrieve");
+	b2.addActionListener(this);
+	b2.setFont(new Font("Tahoma", Font.BOLD, 12));
+	b2.setBounds(426, 188, 85, 29);
+	b2.setBackground(Color.BLACK);
+        b2.setForeground(Color.WHITE);
+	contentPane.add(b2);
+
+	b3 = new JButton("Back");
+	b3.addActionListener(this);
+	b3.setFont(new Font("Tahoma", Font.BOLD, 13));
+	b3.setBounds(233, 270, 101, 29);
+        b3.setBackground(Color.BLACK);
+        b3.setForeground(Color.WHITE);
+	contentPane.add(b3);
+
+
     }
 }
