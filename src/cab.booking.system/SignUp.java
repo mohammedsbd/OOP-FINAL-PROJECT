757cd395994afde2a8ac612cf11af35451dcca94
mohammedsbd <mohammed1112ok@gmail.com>
2025-07-27 07:27:22 -0700

    imports and creat signup handler class

--- src/cab.booking.system/SignUp.java
+++ src/cab.booking.system/SignUp.java
@@ -1,3 +1,16 @@
-public class SignUp {
+package cab.booking.system;
+
+import java.awt.*;//help us cretae graphical usr interface(gui)
+import javax.swing.*;
+import java.sql.*;//to connect to the database
+import java.awt.event.*;//to handle events
+import javax.swing.border.*;//to add border layout
+
+
+
+
+
+
+public class Signup extends JFrame implements ActionListener{
     
 }
