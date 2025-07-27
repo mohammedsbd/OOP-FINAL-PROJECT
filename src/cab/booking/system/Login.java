7a2f19e00e3902ef0625a06be1b1944cf7f4f711
mohammedsbd <mohammed1112ok@gmail.com>
2025-07-27 08:06:22 -0700

    add private methods

--- src/cab/booking/system/Login.java
+++ src/cab/booking/system/Login.java
@@ -5,3 +5,10 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 import java.sql.*;
+
+public class Login extends JFrame implements ActionListener{
+    private JPanel panel;
+	private JTextField textField;
+	private JPasswordField passwordField;
+        private JButton b1,b2,b3;
+}
