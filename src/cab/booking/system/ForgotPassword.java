f58b9f13f8c9742cc5ab805aa6927dedbe876e0b
mohammedsbd <mohammed1112ok@gmail.com>
2025-07-27 08:18:33 -0700

    add fields inside fogot password class

--- src/cab/booking/system/ForgotPassword.java
+++ src/cab/booking/system/ForgotPassword.java
@@ -7,3 +7,9 @@ import javax.swing.*;
 import javax.swing.border.*;
 import java.sql.*;
 import java.awt.event.*;
+
+public class ForgotPassword extends JFrame implements ActionListener{
+    private JPanel contentPane;
+    private JTextField t1,t2,t3,t4,t5;
+    private JButton b1,b2,b3;
+}
