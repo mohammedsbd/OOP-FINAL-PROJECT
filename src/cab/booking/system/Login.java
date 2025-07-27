cddc0b27e82675527a3b3ed0523667c4ce058af2
mohammedsbd <mohammed1112ok@gmail.com>
2025-07-27 08:16:17 -0700

    add main for instance

--- src/cab/booking/system/Login.java
+++ src/cab/booking/system/Login.java
@@ -7,18 +7,22 @@ import java.awt.event.*;
 import java.sql.*;
 
 public class Login extends JFrame implements ActionListener{
-    private JPanel panel;
+
+	private JPanel panel;
 	private JTextField textField;
 	private JPasswordField passwordField;
         private JButton b1,b2,b3;
 
-        public Login(){
-            setBackground(new Color(255, 255, 204));	
+
+	public Login() {
+            
+	setBackground(new Color(255, 255, 204));	
         setBounds(550, 250, 700, 500);
         setLayout(null);
         getContentPane().setBackground(Color.WHITE);
-
-         ImageIcon c1 = new ImageIcon(ClassLoader.getSystemResource("cab/booking/System/icons/login.jpg"));
+        
+        
+        ImageIcon c1 = new ImageIcon(ClassLoader.getSystemResource("cab/booking/System/icons/login.jpg"));
         Image i1 = c1.getImage().getScaledInstance(700, 700,Image.SCALE_DEFAULT);
         ImageIcon i2 = new ImageIcon(i1);
         
@@ -26,7 +30,9 @@ public class Login extends JFrame implements ActionListener{
         JLabel l6 = new JLabel(i2);
         l6.setBounds(0, 50, 700, 450);
         add(l6);
-        JLabel l1 = new JLabel("Username : ");
+        
+
+	JLabel l1 = new JLabel("Username : ");
 	l1.setBounds(324, 0, 95, 24);
 	l6.add(l1);
 
@@ -37,20 +43,20 @@ public class Login extends JFrame implements ActionListener{
 	textField = new JTextField();
 	textField.setBounds(420, 0, 157, 20);
 	l6.add(textField);
-
-    passwordField = new JPasswordField();
+	
+	passwordField = new JPasswordField();
 	passwordField.setBounds(420, 40, 157, 20);
-	l6.add(passwordField)
+	l6.add(passwordField);
 
-    b1 = new JButton("Login");
+	b1 = new JButton("Login");
 	b1.addActionListener(this);
                 
 	b1.setForeground(new Color(46, 139, 87));
 	b1.setBackground(new Color(176, 224, 230));
 	b1.setBounds(324, 80, 113, 25);
 	l6.add(b1);
-
-     b2 = new JButton("SignUp");
+		
+        b2 = new JButton("SignUp");
 	b2.addActionListener(this);
 	
 	b2.setForeground(new Color(139, 69, 19));
@@ -65,15 +71,15 @@ public class Login extends JFrame implements ActionListener{
 	b3.setBounds(360, 120, 179, 25);
 	l6.add(b3);
 
-    	JLabel l5 = new JLabel("Trouble in Login?");
+	JLabel l5 = new JLabel("Trouble in Login?");
 	l5.setFont(new Font("Tahoma", Font.PLAIN, 15));
 	l5.setForeground(new Color(255, 0, 0));
 	l5.setBounds(240, 120, 110, 20);
 	l6.add(l5);
 
-        }
-
-          public void actionPerformed(ActionEvent ae){
+	}
+        
+        public void actionPerformed(ActionEvent ae){
             if(ae.getSource() == b1){
                 Boolean status = false;
 		try {
@@ -107,5 +113,8 @@ public class Login extends JFrame implements ActionListener{
             }
         }
         
+  	public static void main(String[] args) {
+                new Login().setVisible(true);
+	}
 
 }
