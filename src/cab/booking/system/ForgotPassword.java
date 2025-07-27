4b7f1cdcbff8eb24ebc9f66255cdac176922055e
mohammedsbd <mohammed1112ok@gmail.com>
2025-07-27 08:24:52 -0700

    add label1

--- src/cab/booking/system/ForgotPassword.java
+++ src/cab/booking/system/ForgotPassword.java
@@ -12,4 +12,23 @@ public class ForgotPassword extends JFrame implements ActionListener{
     private JPanel contentPane;
     private JTextField t1,t2,t3,t4,t5;
     private JButton b1,b2,b3;
+
+      public static void main(String[] args) {
+	new ForgotPassword().setVisible(true);
+    }
+
+    public ForgotPassword(){
+         setBounds(500, 200, 850, 400);
+	contentPane = new JPanel();
+	contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+	setContentPane(contentPane);
+        contentPane.setBackground(Color.WHITE);
+	contentPane.setLayout(null);
+
+    JLabel l1 = new JLabel("Username");
+	l1.setFont(new Font("Tahoma", Font.BOLD, 13));
+	l1.setBounds(109, 83, 87, 29);
+	contentPane.add(l1);
+
+    }
 }
