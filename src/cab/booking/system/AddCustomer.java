f3c9018e8cf86ebdf041b7df43c1e84af35e27da
aaronkefyalew <aaronkefyalew@gmail.com>
2025-07-29 16:42:49 +1100

    create AddCustomer JFrame class for new customer form

--- src/cab/booking/system/AddCustomer.java
+++ src/cab/booking/system/AddCustomer.java
@@ -1,5 +1,10 @@
 package cab.booking.system;
 
-public class AddCustomer {
-    
-}
+
+import java.awt.*;
+import java.awt.EventQueue;
+
+import java.awt.Font;
+import java.awt.Image;
+import java.sql.*;	
+import javax.swing.*;
