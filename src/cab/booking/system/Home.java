e8b0640fc3a9fbfaaae98c9b3bf5cf4902645385
aaronkefyalew <aaronkefyalew@gmail.com>
2025-07-29 16:19:21 +1100

    Initialize Home JFrame class with main method entry point

---
+++ src/cab/booking/system/Home.java
@@ -0,0 +1,12 @@
+package cab.booking.system;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Home extends JFrame {
+    String username;
+
+    public static void main(String[] args) {
+        new Home("").setVisible(true);
+    }
---
+++ src/cab/booking/system/ViewCustomer.java
@@ -0,0 +1,5 @@
+package cab.booking.system;
+
+public class ViewCustomer {
+    
+}
