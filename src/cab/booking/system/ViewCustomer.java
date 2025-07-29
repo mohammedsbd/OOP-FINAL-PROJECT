f8dbdc174b834bcf9c58ff958f7419ac417f5089
aaronkefyalew <aaronkefyalew@gmail.com>
2025-07-29 17:08:17 +1100

    implement ViewCustomer JFrame to display customer data from database

--- src/cab/booking/system/ViewCustomer.java
+++ src/cab/booking/system/ViewCustomer.java
@@ -1,5 +1,20 @@
 package cab.booking.system;
 
-public class ViewCustomer {
-    
-}
+
+import java.awt.BorderLayout;
+import java.awt.*;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import net.proteanit.sql.DbUtils;
+
+import javax.swing.JTable;
+import java.sql.*;	
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+public class ViewCustomer extends JFrame {
+	Connection conn = null;
+	private JPanel contentPane;
