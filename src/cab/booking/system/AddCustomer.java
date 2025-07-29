b5bbfa1a2ecaa7290198ed946100fb2c4262dca2
aaronkefyalew <aaronkefyalew@gmail.com>
2025-07-29 16:43:44 +1100

    add form fields for username, ID, name, gender, country, address, phone, and email

--- src/cab/booking/system/AddCustomer.java
+++ src/cab/booking/system/AddCustomer.java
@@ -8,3 +8,14 @@ import java.awt.Font;
 import java.awt.Image;
 import java.sql.*;	
 import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+public class AddCustomer extends JFrame {
+	Connection conn = null;
+	PreparedStatement pst = null;
+	private JPanel contentPane;
+	private JTextField t1,t2,t3,t4,t5,t6,t7,t8;
+        JComboBox comboBox;
+        JRadioButton r1,r2;
+        Choice c1;
+	
