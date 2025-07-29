87acfe6e40e8e48bbed3c5496b0d62a3766d04b6
aaronkefyalew <aaronkefyalew@gmail.com>
2025-07-29 17:09:40 +1100

    fetch and populate customer table using DbUtils with SQL query

--- src/cab/booking/system/ViewCustomer.java
+++ src/cab/booking/system/ViewCustomer.java
@@ -18,3 +18,23 @@ import java.awt.event.ActionEvent;
 public class ViewCustomer extends JFrame {
 	Connection conn = null;
 	private JPanel contentPane;
+	private JTable table;
+	private JLabel lblAvailability;
+	private JLabel lblCleanStatus;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JLabel lblRoomNumber;
+	private JLabel lblId;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ViewCustomer frame = new ViewCustomer();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
