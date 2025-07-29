2e001c753bbb9808859981a485ae465afbab78a8
segni53 <segnigobisa12@gmail.com>
2025-07-29 15:03:07 +0300

    create the class for cab viewing

--- src/cab/booking/system/ViewBookedCab.java
+++ src/cab/booking/system/ViewBookedCab.java
@@ -14,3 +14,23 @@ import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.Random;
+
+
+public class ViewBookedCab extends JFrame {
+  private JPanel contentPane;
+        Choice c1;
+  /**
+   * Launch the application.
+   */
+  public static void main(String[] args) {
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        try {
+          ViewBookedCab frame = new ViewBookedCab("");
+          frame.setVisible(true);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
