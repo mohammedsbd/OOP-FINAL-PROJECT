5fc58149fbf533254bf5ecded767100654efa2b4
segni53 <segnigobisa12@gmail.com>
2025-07-29 14:55:25 +0300

    create the class and define fields

--- src/cab/booking/system/BookIntercityCab.java
+++ src/cab/booking/system/BookIntercityCab.java
@@ -6,3 +6,11 @@ import java.awt.event.*;
 import java.sql.*;
 import java.util.Random;
 import javax.swing.*;
+
+
+public class BookIntercityCab extends JFrame{
+     JTable table;
+    JLabel ReservationDetails,Source,Destination;
+    JButton show, b1, b2;
+    Choice c1, c2;
+}
