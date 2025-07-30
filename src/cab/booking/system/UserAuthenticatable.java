ed51311831df54f1ce3420e46e84b017d4490497
mohammedsbd <mohammed1112ok@gmail.com>
2025-07-30 08:08:14 -0700

    add user authinticatable interface

---
+++ src/cab/booking/system/UserAuthenticatable.java
@@ -0,0 +1,8 @@
+// File: src/cab/booking/system/UserAuthenticatable.java
+package cab.booking.system;
+
+public interface UserAuthenticatable {
+    void loginUser();
+    void redirectToSignup();
+    void redirectToForgotPassword();
+}
